5bcf7d18af5aa26c5bb82b503f57f82cfb1f9a84
evgeniy.michurin <anaxaim@gmail.com>
2023-10-27 10:14:53 +0400

    530. Minimum Absolute Difference in BST

---
+++ 530_Minimum_Absolute_Difference_in_BST/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func getMinimumDifference(root *TreeNode) int {
+	minDiff := math.MaxInt32
+	prev := -1
+
+	var inorder func(root *TreeNode)
+	inorder = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+
+		inorder(root.Left)
+		if prev != -1 && root.Val-prev < minDiff {
+			minDiff = root.Val - prev
+		}
+		prev = root.Val
+		inorder(root.Right)
+	}
+	inorder(root)
+
+	return minDiff
+}
+
+func main() {
+	tr1 := &TreeNode{Val: 4, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 1}, Right: &TreeNode{Val: 3}}, Right: &TreeNode{Val: 6}}
+	fmt.Println(getMinimumDifference(tr1)) // 1
+
+	tr2 := &TreeNode{Val: 1, Left: &TreeNode{Val: 0}, Right: &TreeNode{Val: 48, Left: &TreeNode{Val: 12}, Right: &TreeNode{Val: 49}}}
+	fmt.Println(getMinimumDifference(tr2)) // 1
+}
