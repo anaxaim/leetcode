e59c09bd5ac0fd511eb8a11f23a6707fe6933aa5
evgeniy.michurin <anaxaim@gmail.com>
2023-10-28 19:15:09 +0400

    182. Duplicate Emails

---
+++ SQL/182_Duplicate_Emails/.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS Person (id INT, email VARCHAR(255));
+TRUNCATE TABLE Person;
+INSERT INTO Person (id, email) VALUES ('1', 'a@b.com');
+INSERT INTO Person (id, email) VALUES ('2', 'c@d.com');
+INSERT INTO Person (id, email) VALUES ('3', 'a@b.com');
+
+-- 182. Duplicate Emails
+
+SELECT Email
+FROM Person
+GROUP BY email
+HAVING COUNT(Email) > 1
+
+
+-- Input
+-- Person =
+-- | id | email   |
+-- | -- | ------- |
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+--
+-- Output
+-- | Email   |
+-- | ------- |
+-- | a@b.com |
