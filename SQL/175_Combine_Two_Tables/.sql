67ccf6f3a1525c220754f08ccf22c63d29ec29e4
evgeniy.michurin <anaxaim@gmail.com>
2023-10-28 19:03:05 +0400

    181. Employees Earning More Than Their Managers

--- SQL/175_Combine_Two_Tables/.sql
+++ SQL/175_Combine_Two_Tables/.sql
@@ -1,14 +1,35 @@
-CREATE TABLE If NOT EXISTS Person (personId INT, firstName VARCHAR(255), lastName VARCHAR(255))
-CREATE TABLE If NOT EXISTS Address (addressId INT, personId INT, city VARCHAR(255), state VARCHAR(255))
-TRUNCATE TABLE Person
-INSERT INTO Person (personId, lastName, firstName) VALUES ('1', 'Wang', 'Allen')
-INSERT INTO Person (personId, lastName, firstName) VALUES ('2', 'Alice', 'Bob')
-TRUNCATE TABLE Address
-INSERT INTO Address (addressId, personId, city, state) VALUES ('1', '2', 'New York City', 'New York')
-INSERT INTO Address (addressId, personId, city, state) VALUES ('2', '3', 'Leetcode', 'California')
+CREATE TABLE If NOT EXISTS Person (personId INT, firstName VARCHAR(255), lastName VARCHAR(255));
+CREATE TABLE If NOT EXISTS Address (addressId INT, personId INT, city VARCHAR(255), state VARCHAR(255));
+TRUNCATE TABLE Person;
+INSERT INTO Person (personId, lastName, firstName) VALUES ('1', 'Wang', 'Allen');
+INSERT INTO Person (personId, lastName, firstName) VALUES ('2', 'Alice', 'Bob');
+TRUNCATE TABLE Address;
+INSERT INTO Address (addressId, personId, city, state) VALUES ('1', '2', 'New York City', 'New York');
+INSERT INTO Address (addressId, personId, city, state) VALUES ('2', '3', 'Leetcode', 'California');
 
 -- 175. Combine Two Tables
 
 SELECT Person.firstName, Person.lastName, Address.city, Address.state
 FROM Person
 LEFT JOIN Address ON Person.personId = Address.personId
+
+
+-- Input
+-- Person =
+-- | personId | lastName | firstName |
+-- | -------- | -------- | --------- |
+-- | 1        | Wang     | Allen     |
+-- | 2        | Alice    | Bob       |
+--
+-- Address =
+-- | addressId | personId | city          | state      |
+-- | --------- | -------- | ------------- | ---------- |
+-- | 1         | 2        | New York City | New York   |
+-- | 2         | 3        | Leetcode      | California |
+--
+-- Output
+-- | firstName | lastName | city          | state    |
+-- | --------- | -------- | ------------- | -------- |
+-- | Allen     | Wang     | null          | null     |
+-- | Bob       | Alice    | New York City | New York |
+
---
+++ SQL/181_Employees_Earning_More_Than_Their_Managers/.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS Employee (id INT, name VARCHAR(255), salary INT, managerId INT);
+TRUNCATE TABLE Employee;
+INSERT INTO Employee (id, name, salary, managerId) VALUES ('1', 'Joe', '70000', '3');
+INSERT INTO Employee (id, name, salary, managerId) VALUES ('2', 'Henry', '80000', '4');
+INSERT INTO Employee (id, name, salary, managerId) VALUES ('3', 'Sam', '60000', 'None');
+INSERT INTO Employee (id, name, salary, managerId) VALUES ('4', 'Max', '90000', 'None');
+
+-- 181. Employees Earning More Than Their Managers
+
+SELECT e1.name AS Employee
+FROM Employee e1
+JOIN Employee e2 ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary
+
+
+-- Input
+-- Employee =
+-- | id | name  | salary | managerId |
+-- | -- | ----- | ------ | --------- |
+-- | 1  | Joe   | 70000  | 3         |
+-- | 2  | Henry | 80000  | 4         |
+-- | 3  | Sam   | 60000  | null      |
+-- | 4  | Max   | 90000  | null      |
+--
+-- Output
+-- | Employee |
+-- | -------- |
+-- | Joe      |
